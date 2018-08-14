57f052e2c2cc515fe2bdedaf19f1b003a9e6385c
Sam Thiriot <samuel.thiriot@res-ear.ch>
2018-08-14 19:14:38 +0200

    fixed warnings in check()

--- README.md
+++ README.md
@@ -1,2 +1,47 @@
 # direct-probabilistic-pairing
 experiments on generation of structured synthetic populations
+
+# test the git version 
+
+If you want to test or use the development version, you can clone this repository, and:
+
+install the devtools package 
+
+    install.packages("devtools")
+	install.packages("dplyr")
+	install.packages("igraph")
+	
+the first time, from the gosp.dpp directory
+
+	library(devtools)
+	devtools::install()
+	devtools::load_all()
+	source("data-raw/cas1.R")
+	source("data-raw/case2.R")
+
+No !!!
+	devtools::use_data_raw("gosp.dpp")
+
+then from the root of the clone:
+
+    library(devtools)
+    devtools::check("gosp.dpp")
+
+
+# more info
+
+see http://r-pkgs.had.co.nz/check.html
+
+# releasing:
+
+## generate the data
+
+	library(devtools)
+	devtools::install()
+	source("data-raw/cas1.R")
+	source("data-raw/case2.R")
+
+
+use devtools checking: http://r-pkgs.had.co.nz/check.html
+
+see https://www.r-project.org/nosvn/pandoc/devtools.html
--- gosp.dpp/R/plotting.R
+++ gosp.dpp/R/plotting.R
@@ -6,6 +6,14 @@
 #' @author Samuel Thiriot <samuel.thiriot@res-ear.ch> 
 #'
 
+#' Identifies the columns present it two datasets 
+#'
+#' @param t1 a dataframe
+#' @param t2 a dataframe
+#' @return a list of strings
+#' 
+#' @author Samuel Thiriot <samuel.thiriot@res-ear.ch> 
+#'  
 compute_common_columns <- function(t1, t2) {
     common_cols <- intersect(names(t1), names(t2)) 
     common_cols[common_cols != "id"]
@@ -51,6 +59,9 @@ plot.differences <- function(sp, sampleA, sampleB, nameA="A", nameB="B") {
 
 }
 
+#' @importFrom gridExtra grid.arrange
+#' @importFrom ggplot2 geom_histogram
+#' 
 plot.unconstrained <- function(sp, sampleA, sampleB, nameA="A", nameB="B") {
 
     # TODO read https://stackoverflow.com/questions/38507729/recast-in-r-with-sumproduct
@@ -96,33 +107,38 @@ plot.unconstrained <- function(sp, sampleA, sampleB, nameA="A", nameB="B") {
 #' two graphs showing the initial and observed average degrees in both populations A and B
 #' two graphs showing the difference between the expected and measured distribution of probability of degrees for both A and B
 #' 
-#' @param sp an object returned by the matching.generate method
+#' @param x an object returned by the matching.generate method
 #' @param sampleA the original sample for population A
 #' @param sampleB the original sample for population B
 #' @param nameA a meaningfull label for the entity type of population A, such as "dwellings" (default to "A")
 #' @param nameB a meaningfull label for the entity type of population B, such as "households" (default to "B")
 #' @param colorRef the color to be used to plot values passed as parameters (defaults to "darkgray")
 #' @param colorSynthetic the color to be used to plot values measured in the synthetic population (defaults to "blue")
+#' @param ... other parameters will be ignored quietly
 #' @return nothing
 #' 
 #' @examples 
 #' data(cas1)
 #' case.prepared <- matching.prepare(cas1$sample.A, cas1$sample.B, cas1$pdi, cas1$pdj, cas1$pij)
-#' disc <- matching.arbitrate(case.prepared, nA=50000, nB=40000, nubs.A=1, phi.A=0, delta.A=0, gamma=0, delta.B=0, phi.B=0, nu.B=1, verbose=T)
-#' sp <- matching.generate(case=disc, sample.A=cas1$sample.A, sample.B=cas1$sample.B, verbose=T)
-#' plot.errors(sp, cas1$sample.A$sample, cas1$sample.B$sample, "dwellings", "households")
+#' disc <- matching.arbitrate(case.prepared, nA=50000, nB=40000, nu.A=1, phi.A=0, 
+#'                            delta.A=0, gamma=0, delta.B=0, phi.B=0, nu.B=1, verbose=TRUE)
+#' sp <- matching.generate(case=disc, sample.A=cas1$sample.A, sample.B=cas1$sample.B, verbose=TRUE)
+#' plot(sp, cas1$sample.A$sample, cas1$sample.B$sample, "dwellings", "households")
 #'
 #' @export
 #'
 #' @author Samuel Thiriot <samuel.thiriot@res-ear.ch> 
 #'
-#' @importFrom ggplot2 ggplot aes geom_bar xlab ylab ggtitle scale_fill_gradient2 geom_tile 
-#' @importFrom gridExtra grid.arrange'
+#' @importFrom ggplot2 ggplot aes geom_bar xlab ylab ggtitle scale_fill_gradient2 geom_tile scale_fill_manual
+#' @importFrom gridExtra grid.arrange
+#' @importFrom reshape2 melt
 #'
-plot.errors <- function(sp, sampleA, sampleB, nameA="A", nameB="B", colorRef="darkgray", colorSynthetic="blue") {
+plot.dpp_result <- function(x, sampleA, sampleB, nameA="A", nameB="B", colorRef="darkgray", colorSynthetic="blue", ...) {
+
+    sp <- x
 
     # TODO ensure this object is of the right type
-    if (class(sp) != "dpp_result") stop("the data to analyze sp should be the result of a matching.generate call")
+    if (class(sp) != "dpp_result") stop("the data to analyze x should be the result of a matching.generate call")
     if (class(sampleA) != "data.frame") stop("sampleA should be a data frame")
     if (class(sampleB) != "data.frame") stop("sampleB should be a data frame")
     
@@ -132,7 +148,7 @@ plot.errors <- function(sp, sampleA, sampleB, nameA="A", nameB="B", colorRef="da
         relaxation=c(sp$inputs$nu.A, sp$inputs$phi.A, sp$inputs$delta.A, sp$inputs$gamma, sp$inputs$delta.B, sp$inputs$phi.B, sp$inputs$nu.B)
         )
     all_relaxation$parameter <- factor(all_relaxation$parameter, levels=c("nA","fi","pdi/di","pij","pdj/dj","fj","nB"))
-    plot_all_relaxation <- ggplot(all_relaxation, aes(parameter, relaxation)) + geom_bar(stat="identity", fill=colorRef)
+    plot_all_relaxation <- ggplot(all_relaxation, aes(factor(parameter), factor(relaxation))) + geom_bar(stat="identity", fill=colorRef)
 
     # plot errors on each value
     all_errors <- data.frame(
@@ -140,7 +156,7 @@ plot.errors <- function(sp, sampleA, sampleB, nameA="A", nameB="B", colorRef="da
         NRMSE=c(sp$gen$nrmse.nA, sp$gen$nrmse.fi, sp$gen$nrmse.pdi, sp$gen$nrmse.di, sp$gen$nrmse.pij, sp$gen$nrmse.dj, sp$gen$nrmse.pdj, sp$gen$nrmse.fj, sp$gen$nrmse.nB)
         )
     all_errors$error <- factor(all_errors$error, levels=c("nA","fi","pdi","di","pij","dj","pdj","fj","nB"))
-    plot_all_errors <- ggplot(all_errors, aes(error, NRMSE)) + geom_bar(stat="identity", fill=colorSynthetic)
+    plot_all_errors <- ggplot(all_errors, aes(factor(error), factor(NRMSE))) + geom_bar(stat="identity", fill=colorSynthetic)
 
     # this scale will be used for various heatmaps    
     scale_gray_blue <- scale_fill_manual(values=c("darkgray","blue"))
@@ -155,7 +171,7 @@ plot.errors <- function(sp, sampleA, sampleB, nameA="A", nameB="B", colorRef="da
 
     # ggplot(population_sizes, aes(x=type)) + geom_bar(data=parameter, stat="identity", fill=colorRef) 
     # geom_bar(stat="identity", aes(y=parameter, fill=colorRef), position="dodge") +
-    plot_population_sizes <- ggplot(population_sizes, aes(x=type, y=count, fill=state)) + geom_bar(stat="identity", position = 'dodge2') + scale_gray_blue
+    plot_population_sizes <- ggplot(population_sizes, aes(x=factor(type), y=factor(count), fill=factor(state))) + geom_bar(stat="identity", position = 'dodge2') + scale_gray_blue
 
     # compare expected and actual degree
     degrees_A <- data.frame(
@@ -164,7 +180,7 @@ plot.errors <- function(sp, sampleA, sampleB, nameA="A", nameB="B", colorRef="da
         state=c(rep("theoretical",length(sp$inputs$di)), rep("synthetic",length(sp$gen$hat.di)))
         )
     degrees_A$state <- factor(degrees_A$state, levels=c("theoretical","synthetic"))
-    plot_degrees_A <- ggplot(degrees_A, aes(x=attributes, y=average.degree, fill=state)) + geom_bar(stat="identity", position = 'dodge2') + scale_gray_blue + ylab("average degree") + ggtitle(paste("average degree",nameA))
+    plot_degrees_A <- ggplot(degrees_A, aes(x=factor(attributes), y=factor(average.degree), fill=factor(state))) + geom_bar(stat="identity", position = 'dodge2') + scale_gray_blue + ylab("average degree") + ggtitle(paste("average degree",nameA))
 
     degrees_B <- data.frame(
         attributes=c(names(sp$inputs$dj),names(sp$gen$hat.dj)),
@@ -172,7 +188,7 @@ plot.errors <- function(sp, sampleA, sampleB, nameA="A", nameB="B", colorRef="da
         state=c(rep("theoretical",length(sp$inputs$dj)), rep("synthetic",length(sp$gen$hat.dj)))
         )
     degrees_B$state <- factor(degrees_B$state, levels=c("theoretical","synthetic"))
-    plot_degrees_B <- ggplot(degrees_B, aes(x=attributes, y=average.degree, fill=state)) + geom_bar(stat="identity", position = 'dodge2') + scale_gray_blue + ylab("average degree") + ggtitle(paste("average degree",nameB))
+    plot_degrees_B <- ggplot(degrees_B, aes(x=factor(attributes), y=factor(average.degree), fill=factor(state))) + geom_bar(stat="identity", position = 'dodge2') + scale_gray_blue + ylab("average degree") + ggtitle(paste("average degree",nameB))
 
 
     # compare expected and actual frequencies
@@ -182,7 +198,7 @@ plot.errors <- function(sp, sampleA, sampleB, nameA="A", nameB="B", colorRef="da
         state=c(rep("theoretical",length(sp$stats$fi)), rep("synthetic",length(sp$gen$hat.fi)))
         ) 
     frequencies_A$state <- factor(frequencies_A$state, levels=c("theoretical","synthetic"))
-    plot_frequencies_A <- ggplot(frequencies_A, aes(x=attributes, y=frequency, fill=state)) + geom_bar(stat="identity", position = 'dodge2') + scale_gray_blue + ylab("freq") + ggtitle(paste("frequencies",nameA))
+    plot_frequencies_A <- ggplot(frequencies_A, aes(x=factor(attributes), y=factor(frequency), fill=factor(state))) + geom_bar(stat="identity", position = 'dodge2') + scale_gray_blue + ylab("freq") + ggtitle(paste("frequencies",nameA))
 
     frequencies_B <- data.frame(
         attributes=c(names(sp$stats$fj),names(sp$gen$hat.fj)),
@@ -190,7 +206,7 @@ plot.errors <- function(sp, sampleA, sampleB, nameA="A", nameB="B", colorRef="da
         state=c(rep("theoretical",length(sp$stats$fj)), rep("synthetic",length(sp$gen$hat.fj)))
         ) 
     frequencies_B$state <- factor(frequencies_B$state, levels=c("theoretical","synthetic"))
-    plot_frequencies_B <- ggplot(frequencies_B, aes(x=attributes, y=frequency, fill=state)) + geom_bar(stat="identity", position = 'dodge2') + scale_gray_blue + ylab("freq") + ggtitle(paste("frequencies",nameB))
+    plot_frequencies_B <- ggplot(frequencies_B, aes(x=factor(attributes), y=factor(frequency), fill=factor(state))) + geom_bar(stat="identity", position = 'dodge2') + scale_gray_blue + ylab("freq") + ggtitle(paste("frequencies",nameB))
 
 
     heat_map_gradient <- scale_fill_gradient2(limits=c(-1,1)) # , trans="log"
@@ -199,18 +215,18 @@ plot.errors <- function(sp, sampleA, sampleB, nameA="A", nameB="B", colorRef="da
     diff_pdi <- sp$inputs$pdi$data - sp$gen$hat.pdi
     diff_pdi$degree <- factor(seq(0,nrow(diff_pdi)-1))
     data_hm_pdi <- melt(diff_pdi)
-    plot_pdi <- ggplot(data_hm_pdi, aes(variable, degree)) + geom_tile(aes(fill=value)) + heat_map_gradient + ggtitle(paste("difference degree for",nameA))
+    plot_pdi <- ggplot(data_hm_pdi, aes(factor(variable), factor(degree))) + geom_tile(aes(fill=value)) + heat_map_gradient + ggtitle(paste("difference degree for",nameA))
 
     diff_pdj <- sp$inputs$pdj$data - sp$gen$hat.pdj
     diff_pdj$degree <- factor(seq(0,nrow(diff_pdj)-1))
     data_hm_pdj <- melt(diff_pdj)
-    plot_pdj <- ggplot(data_hm_pdj, aes(variable, degree)) + geom_tile(aes(fill=value)) + heat_map_gradient + ggtitle(paste("difference degree for",nameB))
+    plot_pdj <- ggplot(data_hm_pdj, aes(factor(variable), factor(degree))) + geom_tile(aes(fill=value)) + heat_map_gradient + ggtitle(paste("difference degree for",nameB))
     
     # plot pij
     diff_pij <- sp$inputs$pij$data - sp$gen$hat.pij
     diff_pij$attributesB <- row.names(diff_pij)
     data_hm_pij <- melt(diff_pij)
-    plot_pij <- ggplot(data_hm_pij, aes(variable, attributesB)) + geom_tile(aes(fill=value)) + heat_map_gradient + ggtitle(paste("difference peering"))
+    plot_pij <- ggplot(data_hm_pij, aes(factor(variable), factor(attributesB))) + geom_tile(aes(fill=value)) + heat_map_gradient + ggtitle(paste("difference peering"))
 
     grid.arrange(
         plot_all_relaxation, plot_all_errors, 
